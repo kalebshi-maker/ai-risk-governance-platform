2fb0fe8cfd6783da1a76e7592c4020f2ee5e74cf
kalebshi-maker <haochenshi@suumail.net>
2026-04-25 00:45:41 -0600

    Update demo_app.py

--- demo_app.py
+++ demo_app.py
@@ -1,3 +1,4 @@
+https://ai-risk-governance-platform-fpdg6w5kn8qkt4tn8x5xzc.streamlit.app/
 
 import streamlit as st
 import numpy as np
@@ -111,7 +112,7 @@ if status == "🔴 FAILURE":
 
     # 3. Audio (Ensure this is indented exactly like the lines above!)
     # Using a high-reliability link from a different source
-    audio_url = "https://youtu.be/s3VXZR5tgSg"
+    audio_url = "https://gfxsounds.com/wp-content/uploads/2022/12/Futuristic-alarm-or-warning-loopable-2.mp3"
     st.audio(audio_url, format="audio/mp3", autoplay=True)
 # -------------------------
 # SCORE (GAMIFIED)
